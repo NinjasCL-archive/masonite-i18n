f23cfd8e698a3a2c04d2bd4a9517cf344aca2833
Camilo <camilo@ninjas.cl>
2018-08-13 11:39:19 -0300

    improved setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup
 from locale import VERSION
 
 setup(
-    name='Masonite i18n',
+    name='masonite-i18n',
     version=VERSION,
     packages=['locale'],
     license='MIT',
     author='Camilo Castro',
     author_email='camilo@ninjas.cl',
     description='Provides i18n to Masonite',
-    url='http://ninjas.cl',
+    url='https://github.com/clsource/masonite-i18n',
     install_requires=[
-        'masonite',
+        'hjson',
     ],
     keywords=['i18n', 'translation', 'python3', 'masonite'],
     include_package_data=True,
+    classifiers=[]
 )
