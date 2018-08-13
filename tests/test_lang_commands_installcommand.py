0970ab848d52ec5d48fafae33e4cd40c2b720995
Camilo <camilo@ninjas.cl>
2018-08-13 17:47:28 -0300

    removed unused test

--- tests/test_lang_commands_installcommand.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from masonite.app import app
-from lang.commands.InstallCommand import InstallCommand
-
-class TestInstallCommand:
-
-    def setup_method(self):
-        self.app = App()
